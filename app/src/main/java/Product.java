242291f9c294bbebffe3844370f62152fed366d4
LENOVO <huy0000aa@gmail.com>
2022-09-26 20:22:28 +0700

    update

--- app/src/main/java/Product.java
+++ app/src/main/java/Product.java
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
+
 import android.os.Bundle;
 
 public class Product {
-jkbjkbnlknlnln;
 
-    public static Product newInstance() {
-        
-        Bundle args = new Bundle();
-        
-        Product fragment = new Product();
-        fragment.setArguments(args);
-        return fragment;
     }
-=======
-public class Product {
 
->>>>>>> origin/master
-}
