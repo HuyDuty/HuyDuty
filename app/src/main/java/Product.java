f2b28e4a3ea7959cfef3c24bb05da0bf07e23e40
LENOVO <huy0000aa@gmail.com>
2022-09-26 20:19:37 +0700

    ghhfdgfd

---
+++ app/src/main/java/Product.java
@@ -0,0 +1,14 @@
+import android.os.Bundle;
+
+public class Product {
+jkbjkbnlknlnln;
+
+    public static Product newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        Product fragment = new Product();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
